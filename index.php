4c3f45e9b98ef83f52e1b83a9b89d86b7a72b127
Nathan PROVOST <nathan.provost78990@gmail.com>
2020-04-06 14:35:51 +0200

    Version 1.0

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>GITHUB</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <style>
+            *{box-sizing: border-box;}
+            span{color:darkred; font-weight:bold;}
+        </style>
+    </head>
+    <body>
+        <div class="container text-center">
+            <h1 class="display-4 pb-4 mb-4">Mon SITE GITHUB</h1>
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <div class="row mx-0">
+                        <h2>Titre 1</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>FOOTER</span><a href="#" class="mr-4 pr-4">Lien 1</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row mx-0">
+                        <h2>Titre 2</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>FOOTER</span><a href="#" class="mr-4 pr-4">Lien 2</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="row mx-0">
+                        <h2>Titre 3</h2>
+                    </div>
+                    <div class="row mx-0 mt-4">
+                        <p class="text-right">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam odit dicta ipsum asperiores. Exercitationem, animi corporis quos culpa voluptatibus cupiditate dolore sed possimus velit, excepturi magni perferendis harum rerum.</p>
+                    </div>
+                    <div class="row mx-0 d-flex justify-content-between my-2">
+                        <span>FOOTER</span><a href="#" class="mr-4 pr-4">Lien 3</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="col-md-6 bg-danger my-4 display-4">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
